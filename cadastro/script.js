431bba06db38d539ac3b7e62304bfafee5e95ba9
supleme <cesarabreu_@outlook.com>
2022-04-21 18:04:27 -0300

    first commit

---
+++ cadastro/script.js
@@ -0,0 +1,22 @@
+const selecionado = document.querySelector("#flexCheckDefault");
+
+document.getElementById('Cadastrar').addEventListener('click', () => {
+
+    selecionado.addEventListener("change", (el) => {
+        if ((selecionado.checked)) {
+            setTimeout(function () {
+                document.getElementById("carregar").style.display = 'block';
+                window.location.href = "file:///C:/Users/cesar/Desktop/Faculdade/Pepper%20Crasher/C%C3%B3digo/home/index.html"
+                window.location.href = "http://127.0.0.1:5500/home/index.html"
+            }, 1000)
+
+        }
+        if (!(selecionado.checked)) {
+            alert("Você não é maior de idade.");
+            document.getElementById("carregar").style.display = 'none';
+        }
+    });
+
+    selecionado.dispatchEvent(new Event("change"));
+
+});
---
+++ home/script.js
@@ -0,0 +1,24 @@
+confirm('Voce assume os riscos de continuar?')
+var nome = prompt('Qual o seu nome?')
+alert(`Seja bem vindo ${nome}, é um prazer ter você aqui `)
+
+
+var counter = 0;
+
+function alterna(c) {
+    var imgName = document.querySelector("#jogo > img").src;
+    if (imgName.indexOf('frente') > 0) {
+        document.querySelector('#jogo > img').src = 'img/0.gif';
+    } else {
+        document.querySelector('#jogo > img').src = 'img/1.gif'
+    }
+    counter++;
+    console.log(counter);
+    if (counter >= 5) {
+        clearInterval(chave);
+    }
+}
+
+chave = setInterval(alterna, 2000);
+
+document.getElementsByTagName('confirmar').addEventListener('click', alterna)
